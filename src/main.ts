f2554fba056620f66ea00cf361c428a7e7f15326
ryanrixxh <ryanmbr@protonmail.com>
2025-05-17 15:31:38 +0930

    project card and contact page

--- src/main.ts
+++ src/main.ts
@@ -52,6 +52,15 @@ window.Alpine = Alpine
 
 Alpine.store("show_test", false)
 Alpine.store("hello", "Hello World from Alpine!")
+Alpine.store("content", {
+  // TODO: Populate this array from JSON
+  projects: [
+    { name: 'Twitch Timestamper',
+      description: 'A cross platform desktop application for easily saving timestamps whilst livestreaming.',
+      image: 'https://images.unsplash.com/photo-1527334919515-b8dee906a34b?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }
+  ]
+})
 
 document.addEventListener('alpine:init', () => {
   Alpine.data("component", component),
